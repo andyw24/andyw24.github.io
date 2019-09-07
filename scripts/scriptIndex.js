b3680c9293bb2a12bd99ed96b5646655474a36eb
andyw24 <andyw24@gmail.com>
2019-09-07 19:16:08 -0400

    redesign
    - website redesign and update
    - added Debug game to home screen

---
+++ scripts/scriptIndex.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  var bug = true;
+
+  $(".reveal").on('mousemove', function(event) {
+    if (bug) {
+      var upX = event.clientX + $(document).scrollLeft();
+      var upY = event.clientY + $(document).scrollTop();
+      var mask = $('#mask circle')[0];
+      mask.setAttribute("cy", (upY - 5) + 'px');
+      mask.setAttribute("cx", (upX) + 'px');
+    }
+  });
+  var randomX = function() {
+    return Math.floor((Math.random() * 90 + 1));
+  }
+  var randomY = function() {
+    return Math.floor((Math.random() * 90 + 1));
+  }
+
+  var moveBug = function() {
+    $(".bug").animate({ x: randomX() + "%", y: randomY() + "%" }, duration = 5000, function() {
+      moveBug();
+    });
+  }
+
+  var startBug = function() {
+    $(".bug").animate({ x: randomX() + "%", y: randomY() + "%" });
+    moveBug();
+  }
+
+  $(".bug").click(function() {
+    bug = false;
+    $("#mainSvg").remove();
+    $("#compile").css('color', 'lightgreen');
+    $("#compile").text("Compilation completed with 0 error(s)");
+  });
+
+  startBug();
+});
